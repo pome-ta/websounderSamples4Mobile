5035c0777c3467d33a4c4f38c8539c0d9aae0492
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-07-02 11:31:49 +0900

    demo03.js

---
+++ docs/js/demo03.js
@@ -0,0 +1,135 @@
+import { EventWrapper } from './EventWrapper.js';
+
+/* util funcs */
+function capitalize(str) {
+  if (typeof str !== 'string' || !str) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function getDecimalPointDigits(valueStr) {
+  const numbers = valueStr.split('.');
+  const pointLen = numbers[1] ? numbers[1].length : 0;
+  // xxx: 0 で、返したものを1 で返してる
+  return pointLen ? pointLen : 1;
+}
+
+function parseValueNum({ value, step, numtype = 'float' }) {
+  return numtype === 'int'
+    ? Number.parseInt(value)
+    : Number.parseFloat(value).toFixed(getDecimalPointDigits(step));
+}
+
+/* create document node element funcs */
+function createButton(idName, textContent = null) {
+  const element = document.createElement('button');
+  element.style.width = '100%';
+  element.style.height = '4rem';
+  element.type = 'button';
+  element.id = idName;
+  element.textContent = textContent;
+  return element;
+}
+
+function createInputRange({ id, min, max, value, numtype, step = 1 }) {
+  const element = document.createElement('input');
+  element.type = 'range';
+  element.id = id;
+  element.min = min;
+  element.max = max;
+  element.step = step;
+  element.value = value;
+  element.numtype = numtype;
+  element.style.width = '100%';
+  element.style.height = '2rem';
+  element.style.margin = 0;
+  return element;
+}
+
+function setAppendChild(nodes, parentNode = document.body) {
+  let preNode = parentNode;
+  nodes.forEach((node) => {
+    Array.isArray(node)
+      ? setAppendChild(node, preNode)
+      : parentNode.appendChild(node);
+    preNode = node;
+  });
+}
+
+/* setup document node element */
+const mainTitleHeader = document.createElement('h1');
+mainTitleHeader.textContent = 'GainNode';
+
+const buttonWrap = document.createElement('div');
+buttonWrap.style.width = '100%';
+const labelValues = ['Play', 'Pause'];
+const captionPlayPause = document.createTextNode(labelValues.join(' / '));
+const soundButton = createButton('sound', labelValues[0]);
+
+const sliderWrap = document.createElement('div');
+sliderWrap.style.width = '100%';
+const captionSlider = document.createTextNode('Volume: ');
+const sliderValue = document.createElement('span');
+
+const sliderDiv = document.createElement('div');
+sliderDiv.style.width = '88%';
+sliderDiv.style.margin = 'auto';
+
+const sliderRange = createInputRange({
+  id: 'range-volume',
+  min: 0.0,
+  max: 1.0,
+  step: 0.05,
+  value: 0.5,
+  numtype: 'float',
+});
+
+setAppendChild([
+  mainTitleHeader,
+  buttonWrap,
+  [captionPlayPause, soundButton],
+  sliderWrap,
+  [captionSlider, sliderValue],
+  sliderDiv,
+  [sliderRange],
+]);
+
+/* audio */
+// xxx: prefix は無し
+const context = new AudioContext();
+//const oscillator = context.createOscillator();
+let oscillator;
+const gain = context.createGain();
+
+gain.gain.value = sliderRange.valueAsNumber;  // Set volume
+sliderValue.textContent = parseValueNum(sliderRange);
+
+function tapAction() {
+  // xxx: [0, 1] の繰り返しなので、ビット排他的理論和処理。無駄に
+  const isPlayFlag = labelValues.indexOf(this.textContent) ^ 1;
+
+  if (isPlayFlag) {
+    oscillator = context.createOscillator();
+    oscillator.connect(gain);
+    gain.connect(context.destination);
+    oscillator.start(0);
+  } else {
+    oscillator.stop(0);
+  }
+  this.textContent = labelValues[isPlayFlag];
+}
+
+function controlVolume() {
+  // xxx: GLOBAL 定義する？
+  const min = gain.gain.minValue || 0;
+  const max = gain.gain.maxValue || 1;
+  if (this.valueAsNumber >= min && this.valueAsNumber <= max) {
+    gain.gain.value = this.valueAsNumber;
+    sliderValue.textContent = parseValueNum(this);
+  }
+}
+
+/* Event */
+const eventWrap = new EventWrapper();
+soundButton.addEventListener(eventWrap.start, tapAction);
+sliderRange.addEventListener('input', controlVolume);
+

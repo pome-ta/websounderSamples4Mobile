062d509b3cedc02a88281d9c71dc5d80304c1963
pome-ta <pometarou93@gmail.com>
2022-07-05 23:03:54 +0900

    call soud!

--- docs/js/main.js
+++ docs/js/main.js
@@ -29,11 +29,8 @@ const setupDOM = () => {
 
   // SELECT AUDIO FILE
   const selectAudioCaption = document.createTextNode('SELECT AUDIO FILE : ');
-
   selectAudioName = document.createElement('span');
-
   fileUploadAudioButton = createButton('file-upload-audio', 'Upload');
-
   const selectAudioSection = setAppendChild(
     [selectAudioCaption, selectAudioName, fileUploadAudioButton],
     createSection()
@@ -41,13 +38,10 @@ const setupDOM = () => {
 
   // VOLUME
   const volumeRangeCaption = document.createTextNode('VOLUME : ');
-
   volumeRangeValue = document.createElement('span');
-
   const volumeRangeWrap = document.createElement('div');
   volumeRangeWrap.style.width = '88%';
   volumeRangeWrap.style.margin = 'auto';
-
   volumeRange = createInputRange({
     id: 'range-volume',
     min: 0.0,
@@ -56,7 +50,6 @@ const setupDOM = () => {
     value: 1.0,
     numtype: 'float',
   });
-
   const volumeRangeSection = setAppendChild(
     [volumeRangeCaption, volumeRangeValue, volumeRangeWrap, [volumeRange]],
     createSection()
@@ -64,13 +57,10 @@ const setupDOM = () => {
 
   // PLAYBACK RATE
   const playbackRateCaption = document.createTextNode('PLAYBACK RATE : ');
-
   playbackRateValue = document.createElement('span');
-
   const rateRangeWrap = document.createElement('div');
   rateRangeWrap.style.width = '88%';
   rateRangeWrap.style.margin = 'auto';
-
   rateRange = createInputRange({
     id: 'range-playback-rate',
     min: 0.05,
@@ -79,7 +69,6 @@ const setupDOM = () => {
     value: 1.0,
     numtype: 'float',
   });
-
   const playbackRateSection = setAppendChild(
     [playbackRateCaption, playbackRateValue, rateRangeWrap, [rateRange]],
     createSection()
@@ -87,15 +76,12 @@ const setupDOM = () => {
 
   // LOOP
   const loopToggleCaption = document.createTextNode(' LOOP');
-
   loopToggleBox = createCheckbox({
     id: 'loopToggle',
   });
-
   const loopToggleLabel = document.createElement('label');
   loopToggleLabel.htmlFor = loopToggleBox.id;
   loopToggleLabel.style.cursor = 'pointer';
-
   const loopToggleSection = setAppendChild(
     [loopToggleLabel, [loopToggleBox, loopToggleCaption]],
     createSection()
@@ -104,7 +90,6 @@ const setupDOM = () => {
   const stopButtonWrap = document.createElement('div');
   stopButtonWrap.style.padding = '2rem';
   stopButtonWrap.style.margin = 'auto';
-
   stopAudioButton = createButton('stop', '🤚 Stop Audio');
 
   // overall DOM setup
@@ -130,53 +115,27 @@ const context = new AudioContext(); // xxx: prefix
 let source = null;
 const gain = context.createGain();
 
-function _startAudio(arrayBuffer) {
+const startAudio = (arrayBuffer) => {
   source = context.createBufferSource();
-  /*const audioBuffer = context.decodeAudioData(arrayBuffer).then((decodedData) => {
-    //console.log(decodedData);
-    return decodedData;
-  });*/
-
-  //decodeAudioData
-  //const audioBuffer = await LoadBuffer(context, )
-
-  //context.decodeAudioData(arrayBuffer, successCallback, errorCallback);
-  //source.buffer = audioBuffer;
-  //source.buffer = audioBuffer;
-  //source.playbackRate.value = rateRange.valueAsNumber;
-  //source.loop = loopToggleBox.checked;
-
-  // AudioBufferSourceNode (Input) -> GainNode (Volume) -> AudioDestinationNode (Output)
-  //console.log(source);
-  /*
-  context.decodeAudioData(arrayBuffer).then(function (decodedData) {
-    // デコードしたデータをここで使う
-    //console.log(decodedData);
-    source.buffer = decodedData;
-  });*/
-  //console.log(rateRange.valueAsNumber);
-  //loopToggleBox.checked
-  context.decodeAudioData(arrayBuffer, function (decodedData) {
-    source.buffer = decodedData;
-  });
+  context
+    .decodeAudioData(arrayBuffer)
+    .then((decodedData) => (source.buffer = decodedData));
   source.connect(gain);
   gain.connect(context.destination);
-
   // Start audio
   source.start(0);
-  //context.decodeAudioData(arrayBuffer, successCallback, errorCallback);
-}
+  source.onended = function (event) {
+    const uploader = fileUploadAudioButton;
+    uploader.disabled = false;
+    uploader.textContent = 'Upload';
 
-const startAudio = function (arrayBuffer) {
-  source = context.createBufferSource();
-  context.decodeAudioData(arrayBuffer, function (decodedData) {
-    source.buffer = decodedData;
-  });
-  source.connect(gain);
-  gain.connect(context.destination);
+    const progressArea = selectAudioName;
+    progressArea.textContent = '';
 
-  // Start audio
-  source.start(0);
+    source.onended = null;
+    source.stop(0);
+    // console.log(`STOP by 'on ${event.type}' event handler !! `);
+  };
 };
 
 async function eventTargetFile(uri) {
@@ -190,15 +149,11 @@ async function eventTargetFile(uri) {
   return new File([data.blob], uri, { type: data.contentType });
 }
 
-async function LoadBuffer(actx, buffer) {
-  return actx.decodeAudioData(buffer);
-}
-
 /* Events */
 const eventWrap = new EventWrapper();
 
-fileUploadAudioButton.addEventListener(eventWrap.click, async function (event) {
-  const uploader = this;
+fileUploadAudioButton.addEventListener(eventWrap.start, async () => {
+  const uploader = fileUploadAudioButton;
   uploader.disabled = true;
   uploader.textContent = 'Now Loading...';
   const progressArea = selectAudioName;
@@ -224,13 +179,6 @@ fileUploadAudioButton.addEventListener(eventWrap.click, async function (event) {
   reader.readAsArrayBuffer(file);
 });
 
-/*
-document.addEventListener('DOMContentLoaded', async () => {
-  source = context.createBufferSource();
-  const audioBuffer = await LoadSample(context, soundPath);
-  source.buffer = audioBuffer;
-});
-*/
 // Toggle loop
 loopToggleBox.addEventListener(eventWrap.CLICK, function () {
   if (source instanceof AudioBufferSourceNode) {

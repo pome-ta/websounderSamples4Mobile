be4708510645804ca36cbc816bbb708d303dd46e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-07-08 01:30:04 +0900

    foreach

--- docs/js/main.js
+++ docs/js/main.js
@@ -31,189 +31,200 @@ const setupDOM = () => {
   mainTitleHeader.style.fontSize = '0.8rem';
 
   // main controller
-  const mainControlView = document.createElement('article');
-  mainControlView.style.width = '92%';
-  mainControlView.style.margin = '1rem auto';
+  const setupMainControl = () => {
+    // START / PAUSE
+    const captionPlayPause = document.createTextNode(labelValues.join(' / '));
+    playPauseButton = createButton('sound', labelValues[0]);
+    playPauseButton.style.width = '100%';
+    const playPauseWrap = setAppendChild(
+      [captionPlayPause, playPauseButton],
+      createSection()
+    );
 
-  // START / PAUSE
-  const captionPlayPause = document.createTextNode(labelValues.join(' / '));
-  playPauseButton = createButton('sound', labelValues[0]);
-  playPauseButton.style.width = '100%';
+    // VOLUME
+    const volumeRangeCaption = document.createTextNode('VOLUME : ');
+    volumeRangeValue = document.createElement('span');
+    const volumeRangeWrap = document.createElement('div');
+    volumeRangeWrap.style.width = '88%';
+    volumeRangeWrap.style.margin = 'auto';
+    volumeRange = createInputRange({
+      id: 'range-volume',
+      min: 0.0,
+      max: 1.0,
+      step: 0.05,
+      value: 1.0,
+      numtype: 'float',
+    });
+    const volumeRangeSection = setAppendChild(
+      [volumeRangeCaption, volumeRangeValue, volumeRangeWrap, [volumeRange]],
+      createSection()
+    );
 
-  const playPauseWrap = setAppendChild(
-    [captionPlayPause, playPauseButton],
-    createSection()
-  );
+    // PLAYBACK RATE
+    const playbackRateCaption = document.createTextNode('PLAYBACK RATE : ');
+    playbackRateValue = document.createElement('span');
+    const rateRangeWrap = document.createElement('div');
+    rateRangeWrap.style.width = '88%';
+    rateRangeWrap.style.margin = 'auto';
+    rateRange = createInputRange({
+      id: 'range-playback-rate',
+      min: 0.05,
+      max: 2.0,
+      step: 0.05,
+      value: 1.0,
+      numtype: 'float',
+    });
+    const playbackRateSection = setAppendChild(
+      [playbackRateCaption, playbackRateValue, rateRangeWrap, [rateRange]],
+      createSection()
+    );
 
-  // VOLUME
-  const volumeRangeCaption = document.createTextNode('VOLUME : ');
-  volumeRangeValue = document.createElement('span');
-  const volumeRangeWrap = document.createElement('div');
-  volumeRangeWrap.style.width = '88%';
-  volumeRangeWrap.style.margin = 'auto';
-  volumeRange = createInputRange({
-    id: 'range-volume',
-    min: 0.0,
-    max: 1.0,
-    step: 0.05,
-    value: 1.0,
-    numtype: 'float',
-  });
-  const volumeRangeSection = setAppendChild(
-    [volumeRangeCaption, volumeRangeValue, volumeRangeWrap, [volumeRange]],
-    createSection()
-  );
+    // audioControls
+    const audioToggleCaption = document.createTextNode(' Controls');
+    audioToggleBox = createCheckbox({
+      id: 'loopToggle',
+    });
+    const audioToggleLabel = document.createElement('label');
+    audioToggleLabel.htmlFor = audioToggleBox.id;
+    audioToggleLabel.style.cursor = 'pointer';
+    const audioToggleSection = setAppendChild(
+      [audioToggleLabel, [audioToggleBox, audioToggleCaption]],
+      createSection()
+    );
 
-  // PLAYBACK RATE
-  const playbackRateCaption = document.createTextNode('PLAYBACK RATE : ');
-  playbackRateValue = document.createElement('span');
-  const rateRangeWrap = document.createElement('div');
-  rateRangeWrap.style.width = '88%';
-  rateRangeWrap.style.margin = 'auto';
-  rateRange = createInputRange({
-    id: 'range-playback-rate',
-    min: 0.05,
-    max: 2.0,
-    step: 0.05,
-    value: 1.0,
-    numtype: 'float',
-  });
-  const playbackRateSection = setAppendChild(
-    [playbackRateCaption, playbackRateValue, rateRangeWrap, [rateRange]],
-    createSection()
-  );
+    // Audio
+    const audioWrap = document.createElement('div');
+    audioWrap.style.width = '100%';
 
-  // audioControls
-  const audioToggleCaption = document.createTextNode(' Controls');
-  audioToggleBox = createCheckbox({
-    id: 'loopToggle',
-  });
-  const audioToggleLabel = document.createElement('label');
-  audioToggleLabel.htmlFor = audioToggleBox.id;
-  audioToggleLabel.style.cursor = 'pointer';
-  const audioToggleSection = setAppendChild(
-    [audioToggleLabel, [audioToggleBox, audioToggleCaption]],
-    createSection()
-  );
+    audio = new Audio(soundPath);
+    audio.controls = true;
+    audio.style.display = 'block';
+    audio.style.width = '100%';
+    audio.style.margin = 'auto';
+    const audioSection = setAppendChild([audioWrap, [audio]], createSection());
 
-  // Audio
-  const audioWrap = document.createElement('div');
-  audioWrap.style.width = '100%';
-
-  audio = new Audio(soundPath);
-  audio.controls = true;
-  audio.style.display = 'block';
-  audio.style.width = '100%';
-  audio.style.margin = 'auto';
-  const audioSection = setAppendChild([audioWrap, [audio]], createSection());
+    return setAppendChild(
+      [
+        playPauseWrap,
+        volumeRangeSection,
+        playbackRateSection,
+        audioToggleSection,
+        audioSection,
+      ],
+      document.createElement('article')
+    );
+  };
 
   // DELAY controller
-  const delayControlView = document.createElement('article');
-  delayControlView.style.width = '92%';
-  delayControlView.style.margin = '1rem auto';
+  const setupDelayControl = () => {
+    // DELAY
+    const delayRangeCaption = document.createTextNode('DELAY : ');
+    delayRangeValue = document.createElement('span');
+    const delayUnitCaption = document.createTextNode('sec');
+    const delayRangeWrap = document.createElement('div');
+    delayRangeWrap.style.width = '88%';
+    delayRangeWrap.style.margin = 'auto';
+    delayRange = createInputRange({
+      id: 'range-delay-time',
+      min: 0.0,
+      max: 1.0,
+      step: 0.05,
+      value: 0.0,
+      numtype: 'float',
+    });
+    const delayRangeSection = setAppendChild(
+      [
+        delayRangeCaption,
+        delayRangeValue,
+        delayUnitCaption,
+        delayRangeWrap,
+        [delayRange],
+      ],
+      createSection()
+    );
 
-  // DELAY
-  const delayRangeCaption = document.createTextNode('DELAY : ');
-  delayRangeValue = document.createElement('span');
-  const delayUnitCaption = document.createTextNode('sec');
-  const delayRangeWrap = document.createElement('div');
-  delayRangeWrap.style.width = '88%';
-  delayRangeWrap.style.margin = 'auto';
-  delayRange = createInputRange({
-    id: 'range-delay-time',
-    min: 0.0,
-    max: 1.0,
-    step: 0.05,
-    value: 0.0,
-    numtype: 'float',
+    // DRY
+    const dryRangeCaption = document.createTextNode('DRY : ');
+    dryRangeValue = document.createElement('span');
+    const dryRangeWrap = document.createElement('div');
+    dryRangeWrap.style.width = '88%';
+    dryRangeWrap.style.margin = 'auto';
+    dryRange = createInputRange({
+      id: 'range-delay-dry',
+      min: 0.0,
+      max: 1.0,
+      step: 0.05,
+      value: 1.0,
+      numtype: 'float',
+    });
+    const dryRangeSection = setAppendChild(
+      [dryRangeCaption, dryRangeValue, dryRangeWrap, [dryRange]],
+      createSection()
+    );
+
+    // WET
+    const wetRangeCaption = document.createTextNode('WET : ');
+    wetRangeValue = document.createElement('span');
+    const wetRangeWrap = document.createElement('div');
+    wetRangeWrap.style.width = '88%';
+    wetRangeWrap.style.margin = 'auto';
+    wetRange = createInputRange({
+      id: 'range-delay-wet',
+      min: 0.0,
+      max: 1.0,
+      step: 0.05,
+      value: 0.0,
+      numtype: 'float',
+    });
+    const wetRangeSection = setAppendChild(
+      [wetRangeCaption, wetRangeValue, wetRangeWrap, [wetRange]],
+      createSection()
+    );
+
+    // FEEDBACK
+    const feedbackRangeCaption = document.createTextNode('FEEDBACK : ');
+    feedbackRangeValue = document.createElement('span');
+    const feedbackRangeWrap = document.createElement('div');
+    feedbackRangeWrap.style.width = '88%';
+    feedbackRangeWrap.style.margin = 'auto';
+    feedbackRange = createInputRange({
+      id: 'range-delay-feedback',
+      min: 0.0,
+      max: 1.0,
+      step: 0.05,
+      value: 0.0,
+      numtype: 'float',
+    });
+    const feedbackRangeSection = setAppendChild(
+      [
+        feedbackRangeCaption,
+        feedbackRangeValue,
+        feedbackRangeWrap,
+        [feedbackRange],
+      ],
+      createSection()
+    );
+
+    return setAppendChild(
+      [
+        delayRangeSection,
+        dryRangeSection,
+        wetRangeSection,
+        feedbackRangeSection,
+      ],
+      document.createElement('article')
+    );
+  };
+
+  const mainControlView = setupMainControl();
+  const delayControlView = setupDelayControl();
+
+  [mainControlView, delayControlView].forEach((views) => {
+    views.style.width = '92%';
+    views.style.margin = '1rem auto';
   });
-  const delayRangeSection = setAppendChild(
-    [
-      delayRangeCaption,
-      delayRangeValue,
-      delayUnitCaption,
-      delayRangeWrap,
-      [delayRange],
-    ],
-    createSection()
-  );
-
-  // DRY
-  const dryRangeCaption = document.createTextNode('DRY : ');
-  dryRangeValue = document.createElement('span');
-  const dryRangeWrap = document.createElement('div');
-  dryRangeWrap.style.width = '88%';
-  dryRangeWrap.style.margin = 'auto';
-  dryRange = createInputRange({
-    id: 'range-delay-dry',
-    min: 0.0,
-    max: 1.0,
-    step: 0.05,
-    value: 1.0,
-    numtype: 'float',
-  });
-  const dryRangeSection = setAppendChild(
-    [dryRangeCaption, dryRangeValue, dryRangeWrap, [dryRange]],
-    createSection()
-  );
-
-  // WET
-  const wetRangeCaption = document.createTextNode('WET : ');
-  wetRangeValue = document.createElement('span');
-  const wetRangeWrap = document.createElement('div');
-  wetRangeWrap.style.width = '88%';
-  wetRangeWrap.style.margin = 'auto';
-  wetRange = createInputRange({
-    id: 'range-delay-wet',
-    min: 0.0,
-    max: 1.0,
-    step: 0.05,
-    value: 0.0,
-    numtype: 'float',
-  });
-  const wetRangeSection = setAppendChild(
-    [wetRangeCaption, wetRangeValue, wetRangeWrap, [wetRange]],
-    createSection()
-  );
-
-  // FEEDBACK
-  const feedbackRangeCaption = document.createTextNode('FEEDBACK : ');
-  feedbackRangeValue = document.createElement('span');
-  const feedbackRangeWrap = document.createElement('div');
-  feedbackRangeWrap.style.width = '88%';
-  feedbackRangeWrap.style.margin = 'auto';
-  feedbackRange = createInputRange({
-    id: 'range-delay-feedback',
-    min: 0.0,
-    max: 1.0,
-    step: 0.05,
-    value: 0.0,
-    numtype: 'float',
-  });
-  const feedbackRangeSection = setAppendChild(
-    [
-      feedbackRangeCaption,
-      feedbackRangeValue,
-      feedbackRangeWrap,
-      [feedbackRange],
-    ],
-    createSection()
-  );
-
   // overall DOM setup
-  setAppendChild([
-    mainTitleHeader,
-    mainControlView,
-    [
-      playPauseWrap,
-      volumeRangeSection,
-      playbackRateSection,
-      audioToggleSection,
-      audioSection,
-    ],
-    delayControlView,
-    [delayRangeSection, dryRangeSection, wetRangeSection, feedbackRangeSection],
-  ]);
+  setAppendChild([mainTitleHeader, mainControlView, delayControlView]);
 };
 
 setupDOM();
@@ -318,18 +329,10 @@ const eventWrap = new EventWrapper();
 
 // Audio Controller is visible ?
 audioToggleBox.addEventListener(eventWrap.click, updateControllers);
-// Control Master Volume
-volumeRange.addEventListener('input', updateControllers);
-// Control playbackRate
-rateRange.addEventListener('input', updateControllers);
-// Control Delay Time
-delayRange.addEventListener('input', updateControllers);
-// Control Delay Dry
-dryRange.addEventListener('input', updateControllers);
-// Control Delay Wet
-wetRange.addEventListener('input', updateControllers);
-// Control Delay Feedback
-feedbackRange.addEventListener('input', updateControllers);
+
+[volumeRange, rateRange, delayRange, dryRange, wetRange, feedbackRange].forEach(
+  (slider) => slider.addEventListener('input', updateControllers)
+);
 
 playPauseButton.addEventListener(eventWrap.start, function () {
   //context.state === 'suspended' ? context.resume() : null;  // xxx: 読み込みでラグる
